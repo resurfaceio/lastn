73ee26dbe18792000e8fdf8dd99544069022c474
Rob Dickinson <robfromboulder@gmail.com>
2016-02-04 02:09:44 -0700

    Add queue & lock

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 // Copyright (c) 2016 Resurface Labs LLC, All Rights Reserved
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 
@@ -7,7 +10,37 @@ public class Main {
 
     public static void main(String[] args) {
         port(Integer.valueOf(System.getenv("PORT")));
-        get("/hello", (req, res) -> "Hello World from resurfaceio-lastn");
+
+        LinkedList<String> queue = new LinkedList<>();
+        ReentrantReadWriteLock queueLock = new ReentrantReadWriteLock();
+
+        get("/read", (req, res) -> {
+            StringBuilder sb = new StringBuilder();
+            queueLock.readLock().lock();
+            try {
+                for (String json : queue) {
+                    sb.append("<p><code>");
+                    sb.append(json);
+                    sb.append("</code></p>");
+                }
+            } finally {
+                queueLock.readLock().unlock();
+            }
+            return sb.toString();
+        });
+
+        get("/write", (req, res) -> {
+            queueLock.writeLock().lock();
+            try {
+                if (queue.size() == LIMIT) queue.removeLast();
+                queue.addFirst(String.valueOf(System.currentTimeMillis()));
+                return "OK";
+            } finally {
+                queueLock.writeLock().unlock();
+            }
+        });
+
     }
 
+    private static final int LIMIT = 5;
 }
